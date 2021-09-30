4a26ffc46ce04b072a682136c36915591e017e33
rock-fever <amitssrawat2000@gmail.com>
2021-09-30 17:04:24 +0530

    First Iteration

---
+++ index.js
@@ -0,0 +1,153 @@
+var exphbs = require('express-handlebars')
+const port = 5001 || process.env.PORT;
+const express = require('express')
+const path = require('path')
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var morgan = require("morgan");
+var firebase = require("firebase");
+const { format } = require('path');
+const app = express()
+
+const session_time = 1000 * 60 * 60 * 24 * 1;
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDD6ZpbrX17ySGVrH8w0wYDqvGyMXYnPlo",
+  authDomain: "kritika-a12ae.firebaseapp.com",
+  projectId: "kritika-a12ae",
+  storageBucket: "kritika-a12ae.appspot.com",
+  messagingSenderId: "1074719455050",
+  appId: "1:1074719455050:web:42703119681390e56033b3",
+  measurementId: "G-T7X5WRL1GS"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+db.settings({ timestampsInSnapshots: true });
+
+//retrieve
+var user_n, pass, users = [{ id: null, name: null, password: null }];
+var docRef = db.collection('admin').doc('password').collection('credentials').onSnapshot((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    user_n = doc.data().username,
+      pass = doc.data().password;
+    console.log(user_n, pass);
+    users[0].id = 1;
+    users[0].name = user_n;
+    users[0].password = pass;
+  });
+});
+
+
+app.use(morgan("dev"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(cookieParser());
+
+app.use(
+  session({
+    name: 'sid',
+    resave: false,
+    saveUninitialized: false,
+    secret: 'sid_the_sloth',
+    cookie: {
+      maxAge: session_time,
+      sameSite: true,
+      secure: false,
+    },
+
+  })
+);
+
+const redirectLogin = (req, res, next) => {
+  if (!req.session.userId)
+    res.redirect("/")
+
+  else
+    next();
+
+}
+
+const redirectHome = (req, res, next) => {
+  if (req.session.userId)
+    res.redirect("/form")
+
+  else
+    next();
+
+}
+
+app.get('/', redirectHome, (req, res) => {
+  const { userId } = req.session;
+  console.log(req.session);
+  if (userId)
+    window.location.href = "/form";
+  else
+    res.render('data', {
+      style: 'data.css'
+    });
+})
+
+app.get('/form', redirectLogin, (req, res) => {
+  res.render('form', {
+    style: 'form.css',
+  });
+})
+app.get('/newForm', redirectLogin, (req, res) => {
+  res.render('createNew', {
+    style: 'newForm.css'
+  });
+})
+app.get('/userForm', (req, res) => {
+  res.render('user_form', {
+    style: 'newForm.css'
+  });
+})
+app.get('/response', (req, res) => {
+  res.render('response', {
+    style: 'newForm.css'
+  });
+})
+
+
+
+
+
+app.post('/', redirectHome, (req, res) => {
+  const { name, password } = req.body;
+  console.log(name, password);
+  if (name && password) {
+    console.log(name, password);
+    const user = users.find(user => user.name === name && user.password === password)
+    if (user) {
+      req.session.userId = user.id;
+      res.redirect('/form');
+    }
+    else
+      res.redirect('/');
+  }
+
+})
+app.post('/logout', redirectLogin, (req, res) => {
+  req.session.destroy(err => {
+    if (err)//error destroying cookie
+      return res.redirect('/');
+    res.clearCookie('sid');
+    res.redirect('/');
+  })
+
+
+})
+module.exports = app
+
+app.engine('handlebars',exphbs());
+app.set('view engine','handlebars');
+
+app.use(express.static(path.join(__dirname,"static")))
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
